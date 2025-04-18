44c7afca47af7cf191190f3282b9b3592c3e28f4
fred <domaine75@gmail.com>
2025-04-18 09:22:10 +0200

    add code from AmazonQ

---
+++ ImageduplicatefinderUI/DuplicateGroup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ImageduplicatefinderUI
+{
+public partial class MainWindow
+  {
+    public class DuplicateGroup
+    {
+      public string GroupInfo { get; set; }
+      public ObservableCollection<DuplicateImage> DuplicateFiles { get; set; }
+    }
+
+  }
+}
---
+++ ImageduplicatefinderUI/DuplicateImage.cs
@@ -0,0 +1,13 @@
+﻿namespace ImageduplicatefinderUI
+{
+public partial class MainWindow
+  {
+    public class DuplicateImage
+    {
+      public string ImagePath { get; set; }
+      public string FileName { get; set; }
+      public bool IsSelectedForDeletion { get; set; }
+    }
+
+  }
+}
